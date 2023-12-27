f3d2cb93595f032063577d43d8491363ac855f1c
Richard Lee <r7568436@gmail.com>
2023-12-27 15:54:30 +0800

    try to fix create

--- todoList/wwwroot/js/ToDoList.js
+++ todoList/wwwroot/js/ToDoList.js
@@ -1,13 +1,16 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     fetchToDoLists();
+    /*createToDoList();*/
 });
 
 
 function fetchToDoLists() {
-    fetch('/api/todolist')
-        .then(Response => Response.json())
-        .then(data => displayToDoList(data))
+    
+    fetch('/api/todolist')      
+        .then(response => response.json())
+        .then(data => displayToDoList(data))      
         .catch(error => console.error('Unable to retrieve todolists.', error));
+       
 }
 
 
@@ -17,23 +20,74 @@ function displayToDoList(data) {
 
     data.forEach(item => {
         let row = `<tr>
-            <td><input type="checkbox" name="selectedCategory" ${item.isChecked ? 'checked' : ''} /></td>
+            <td><input type="checkbox" name="selectedCategory" ${item.isComplete ? 'checked' : ''} /></td>
             <td>${item.name}</td>
-            <td>${item.displayOrder}</td>
-            <td>${item.startTime}</td>
-            <td>${item.completeTime}</td>
+            <td>${item.priority}</td>
+            <td>${item.created_at}</td>
+            <td>${item.complete_at}</td>
             <td>
                 <div class="w-10 btn-group" role="group">
-                    <button class="btn btn-primary mx-2" onclick="updateTodolist(${item.id})">Edit</button>
-                    <button class="btn btn-danger mx-2" onclick="deleteTodolist(${item.id})">Delete</button>
+                    <button class="btn btn-primary mx-2" onclick="updateTodolist(${item.Id})">Edit</button>
+                    <button class="btn btn-danger mx-2" onclick="deleteTodolist(${item.Id})">Delete</button>
                 </div>
             </td>
         </tr>`;
         tableBody.innerHTML += row;
     });
+
+    
+    document.querySelectorAll('input[name="selectedCategory"]').forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+            if (this.checked) {
+               
+                this.disabled = true;
+            }
+        });
+    });
 }
 
 
-function creatToDoList() {
 
+/*function createToDoList() {
+    const nameInput = document.getElementById('recipient-name');
+    const priorityInput = document.getElementById('message-text');
+    const currentDate = new Date(); 
+
+    const data = {
+        Account_ID: 2,
+        Name: nameInput.value,
+        Priority: parseInt(priorityInput.value),
+        Created_at: currentDate.toISOString(),
+        Complete_at: null,
+        IsComplete: false,
+    };
+
+    fetch('/api/todolist', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('網路請求錯誤');
+            }
+            return response.json();
+        })
+        .then(addedItem => {
+            console.log('添加成功:', addedItem);
+            fetchTodolists();
+            alert('TodoList build success！');
+        })
+        .catch(error => {
+            console.error('創建 TodoList 項目失敗:', error);
+        });
+    
 }
+
+
+postButton = document.getElementById('sendButton');
+postButton.addEventListener('click', createToDoList);
+*/
+
